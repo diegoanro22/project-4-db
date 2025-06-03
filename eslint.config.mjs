99e0afd1691329dfe9ee35c7cf771ddaab92ad44
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-06-02 22:30:21 -0600

    feat: :construction_worker: update ESLint configuration and add Prettier integration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,96 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+const ignorePatterns = [
+  '*.config.js',
+  '*.config.ts',
+  '*.config.mjs',
+];
+
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  { ignores: ignorePatterns },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    languageOptions: {
+      parser: await import('@typescript-eslint/parser'),
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      // Base rules
+      curly: 'error',
+      eqeqeq: ['error', 'always'],
+
+      // TypeScript enhanced rules
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/no-misused-promises': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+
+      // Naming conventions
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'variable',
+          format: ['camelCase', 'PascalCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'typeLike',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'import',
+          format: ['camelCase', 'PascalCase'],
+        },
+        {
+          selector: 'function',
+          format: ['camelCase', 'PascalCase'],
+        },
+      ],
+
+      // Code organization
+      'padding-line-between-statements': [
+        'error',
+        // Variable declarations
+        { blankLine: 'any', prev: ['const', 'let', 'var'], next: '*' },
+        { blankLine: 'any', prev: '*', next: ['const', 'let', 'var'] },
+
+        // Functions
+        { blankLine: 'always', prev: '*', next: 'function' },
+        { blankLine: 'always', prev: 'function', next: '*' },
+
+        // Classes
+        { blankLine: 'always', prev: '*', next: 'class' },
+        { blankLine: 'always', prev: 'class', next: '*' },
+
+        // Imports/exports
+        { blankLine: 'always', prev: 'import', next: '*' },
+        { blankLine: 'any', prev: 'import', next: 'import' },
+        { blankLine: 'always', prev: 'export', next: '*' },
+        { blankLine: 'any', prev: 'export', next: 'export' },
+      ],
+
+      // Prettier integration
+      'prettier/prettier': 'error',
+    },
+  },
+  ...compat.config({
+    extends: ['plugin:prettier/recommended'],
+  }),
 ];
 
 export default eslintConfig;
